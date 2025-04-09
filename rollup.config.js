82d1ff4d4dd47cd8a9eb2d7f65685120a07f6fa7
rizkinov <148356406+rizkinov@users.noreply.github.com>
2025-04-10 02:23:46 +0800

    Prepare for Vercel deployment by fixing module system

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,14 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
-import { defineConfig } from 'rollup';
-import { createRequire } from 'module';
-import babel from '@rollup/plugin-babel';
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('@rollup/plugin-typescript');
+const peerDepsExternal = require('rollup-plugin-peer-deps-external');
+const postcss = require('rollup-plugin-postcss');
+const { defineConfig } = require('rollup');
+const babel = require('@rollup/plugin-babel');
 
-const require = createRequire(import.meta.url);
 const packageJson = require('./package.json');
 
-export default defineConfig([
+module.exports = defineConfig([
   {
     input: 'src/index.ts',
     output: [
@@ -57,7 +55,7 @@ export default defineConfig([
       }),
       postcss({
         config: {
-          path: './postcss.config.cjs',
+          path: './postcss.config.js',
         },
         extensions: ['.css'],
         minimize: true,
